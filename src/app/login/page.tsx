0b7d4113115ef154b22461f853fbc90a548ecda9
OMPedr0_ <pedrogil5a9@gmail.com>
2023-10-15 12:30:41 +0100

    att

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 import { AuthProvider } from "@/api/auth";
 import LoginUser from "@/api/login/page";
 import React from "react";
-import Background from "../components/bg/Background";
+import Background from "../components/bg/page";
 
 export default function Login() {
   return (
