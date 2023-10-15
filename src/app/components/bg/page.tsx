ce7f979ef3d068030b3fa7ee99347b63f4c8c694
OMPedr0_ <pedrogil5a9@gmail.com>
2023-10-15 12:39:54 +0100

    att

--- src/app/components/bg/page.tsx
+++ src/app/components/bg/page.tsx
@@ -9,7 +9,7 @@ const Background: React.FC<BackgroundProps> = ({ imageURL, children }) => {
   return (
     <div className="bg-gradient-to-b from-green1 to-green2 h-screen relative">
       <img
-        src="/bg.png"
+        src="https://firebasestorage.googleapis.com/v0/b/green-team-hackatoon.appspot.com/o/bg.png?alt=media&token=cd3fe099-c15c-4d48-a46a-1a1b5d3c80af&_gl=1*1buekgm*_ga*MjAzOTQzNDExMi4xNjkyNzA2OTI1*_ga_CW55HF8NVT*MTY5NzM2OTkyOS4yNS4xLjE2OTczNjk5NDguNDEuMC4w"
         alt="Background Image"
         className="w-screen h-screen object-cover"
       />
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import Background from "../components/bg/page";
 
 export default function Login() {
   return (
-    <Background imageURL="/bg.png">
+    <Background imageURL="https://firebasestorage.googleapis.com/v0/b/green-team-hackatoon.appspot.com/o/bg.png?alt=media&token=cd3fe099-c15c-4d48-a46a-1a1b5d3c80af&_gl=1*1buekgm*_ga*MjAzOTQzNDExMi4xNjkyNzA2OTI1*_ga_CW55HF8NVT*MTY5NzM2OTkyOS4yNS4xLjE2OTczNjk5NDguNDEuMC4w">
       <AuthProvider>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <LoginUser />
