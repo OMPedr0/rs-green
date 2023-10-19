3fe1acfce0a1cd08c5d7cbd41558cf988f1cc20b
OMPedr0_ <pedrogil5a9@gmail.com>
2023-10-19 13:29:01 +0100

    Update page.tsx

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -46,7 +46,7 @@ export default function Feed() {
 
 
   return (
-    <BackgroundFeed imageURL="https://firebasestorage.googleapis.com/v0/b/rs-green.appspot.com/o/bgfeed.png?alt=media&token=4b319733-4e93-481c-9fbd-2e1a799112f0&_gl=1*ofljg4*_ga*MjAzOTQzNDExMi4xNjkyNzA2OTI1*_ga_CW55HF8NVT*MTY5NzcwOTI2MS4yOS4xLjE2OTc3MDkyODIuMzkuMC4w">
+    <BackgroundFeed >
       <div className="relative">
         <NavBar />
 
