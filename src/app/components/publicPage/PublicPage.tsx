42f08037135dd02f62c6b26f3177d92b355f6b60
OMPedr0_ <pedrogil5a9@gmail.com>
2023-10-19 13:55:12 +0100

    Update PublicPage.tsx

--- src/app/components/publicPage/PublicPage.tsx
+++ src/app/components/publicPage/PublicPage.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React from 'react';
-import { useRouter } from 'next/navigation'; // Correct the import
+import { useRouter } from 'next/navigation'; // Corrija o import se necessário
 
 export function PublicPage() {
   const router = useRouter();
@@ -11,16 +11,10 @@ export function PublicPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-bgpage to-bgpage1 flex flex-col text-white items-center">
+    <div className="min-h-screen bg-gradient-to-b from-bgpage to-bgpage1 flex flex-col text-white">
       <div className="p-4 text-center">
         <h1 className="text-4xl font-extrabold">Rs Green</h1>
       </div>
-      <button
-            onClick={handleLoginClick}
-            className="bg-blue hover:bg-blue1 text-white font-bold py-2 px-4 rounded absolute top-0 right-0 m-4"
-          >
-            Go to Login
-          </button>
 
       <div className="flex-1 flex flex-col lg:flex-row items-center justify-center p-6 space-y-6 lg:space-y-0">
         <div className="lg:w-1/2 p-6">
@@ -33,6 +27,12 @@ export function PublicPage() {
             <li>Obtenha soluções para problemas de doenças nas plantas.</li>
             <li>Descubra informações sobre plantas e ervas medicinais.</li>
           </ul>
+          <button
+        onClick={handleLoginClick}
+        className="bg-blue hover:bg-blue1 text-white font-bold mt-4 py-2 px-4 rounded self-start"
+      >
+        Go to Login
+      </button>
         </div>
         <div className="lg:w-1/2 p-6">
           <img
