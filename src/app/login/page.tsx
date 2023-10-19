3499728be386b428a4f4c32bfea78107bf66864f
OMPedr0_ <pedrogil5a9@gmail.com>
2023-10-19 13:22:26 +0100

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import Background from "../components/bgtodos/bg/page";
 
 export default function Login() {
   return (
-    <Background imageURL="https://firebasestorage.googleapis.com/v0/b/green-team-hackatoon.appspot.com/o/bg.png?alt=media&token=cd3fe099-c15c-4d48-a46a-1a1b5d3c80af&_gl=1*1buekgm*_ga*MjAzOTQzNDExMi4xNjkyNzA2OTI1*_ga_CW55HF8NVT*MTY5NzM2OTkyOS4yNS4xLjE2OTczNjk5NDguNDEuMC4w">
+    <Background imageURL="https://firebasestorage.googleapis.com/v0/b/rs-green.appspot.com/o/bglogin.png?alt=media&token=cc802fac-aebc-4bf1-90f4-2e7e87ef2983&_gl=1*1fk2obq*_ga*MjAzOTQzNDExMi4xNjkyNzA2OTI1*_ga_CW55HF8NVT*MTY5NzcwNTM0MC4yOC4xLjE2OTc3MDU0MzYuNDEuMC4w">
       <AuthProvider>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <LoginUser />
